486fe167b11ec75e71fcd1660bac0896e30ca966
nadaAbedAlkreem <elkaloutnada@gmail.com>
2025-11-14 21:03:36 +0200

    fix(chatting): fix update track messgae

--- app/Http/Requests/api/consultation/StoreMessageRequest.php
+++ app/Http/Requests/api/consultation/StoreMessageRequest.php
@@ -24,6 +24,7 @@ class StoreMessageRequest extends FormRequest
             if (!$chat) {
                 return;
             }
+             dd($chat->status != 'active' || $chat->status != 'accepted'  . $chat ) ;
             if ($chat->status != 'active' || $chat->status != 'accepted') {
                 $validator->errors()->add(
                     'status',
