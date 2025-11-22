d0fc2e42b79d5d021851a7e132d715b53de72c5a
nadaAbedAlkreem <elkaloutnada@gmail.com>
2025-11-22 16:25:51 +0200

    feat(consultant) : edit limit messages

--- app/Http/Controllers/Api/Consultation/MessageController.php
+++ app/Http/Controllers/Api/Consultation/MessageController.php
@@ -36,7 +36,7 @@ class MessageController extends Controller
             }
             $messages = Message::where('chat_request_id', $chatRequestId)
                 ->with(['sender', 'receiver'])
-                ->orderBy('created_at', 'desc')
+                ->orderBy('created_at', 'asc')
                 ->paginate($limit);
             return $this->successResponse(__('messages.DATA_RETRIEVED_SUCCESSFULLY'), MessageResource::collection($messages), 200);
         }catch (Exception $exception)
