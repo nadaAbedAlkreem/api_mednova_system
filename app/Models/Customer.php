3c242ab8f22b784955ed966e1d7c56240757b725
nadaAbedAlkreem <elkaloutnada@gmail.com>
2025-12-04 15:06:24 +0200

    fix(webhook)

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -10,7 +10,6 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Laravel\Sanctum\HasApiTokens;
 
-#[UseFactory(CustomerFactory::class)]
 class Customer extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\CustomerFactory> */
