2d1ad9977e40c34542e1440c0487079af511ae6e
nadaAbedAlkreem <elkaloutnada@gmail.com>
2025-12-04 14:29:26 +0200

    fix(webhook)

--- database/migrations/2025_12_04_122802_edit_approved_in_row_of_consultation_video_requests.php
+++ database/migrations/2025_12_04_122802_edit_approved_in_row_of_consultation_video_requests.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('row_of_consultation_video_requests', function (Blueprint $table) {
+        Schema::table('consultation_video_requests', function (Blueprint $table) {
             $table->boolean('consultant_approved')->nullable()->change(); // هل اعتمد المستشار الاستشارة
             $table->boolean('patient_approved')->nullable()->change();    // هل اعتمد المريض الاستشارة
         });
@@ -22,7 +22,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('row_of_consultation_video_requests', function (Blueprint $table) {
+        Schema::table('consultation_video_requests', function (Blueprint $table) {
             //
         });
     }
