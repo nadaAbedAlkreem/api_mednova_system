153b11c29328ff4e8f1d58e3fa2cb61eb589d32c
nadaAbedAlkreem <elkaloutnada@gmail.com>
2026-01-04 19:33:35 +0200

    update configration

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,10 @@
 <?php
 
 
+use App\Http\Controllers\Api\Payment\WalletTopUpController;
 use Illuminate\Support\Facades\Route;
 
 
 
 
+Route::post('/amwalpay/callback', [WalletTopUpController::class, 'captureDataViaWebhook']);
