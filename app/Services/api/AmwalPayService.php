78b9b3f4a8ac4e995c5a166d9ce17d8f673986d2
nadaAbedAlkreem <elkaloutnada@gmail.com>
2026-01-07 14:33:35 +0200

    update configration

--- app/Services/api/AmwalPayService.php
+++ app/Services/api/AmwalPayService.php
@@ -218,7 +218,7 @@ class AmwalPayService
 
     private function generateSecureHash(array $payload): string
     {
-        unset($payload['SecureHash']);
+        unset($payload['secureHashValue']);
 
         ksort($payload);
 
