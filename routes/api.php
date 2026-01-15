0084e9b0904566ea561183808a64a1df81f43089
nadaAbedAlkreem <elkaloutnada@gmail.com>
2026-01-15 14:28:05 +0200

    update configration

--- routes/api.php
+++ routes/api.php
@@ -216,12 +216,15 @@ Route::prefix('zoom-webhook')->group(function ()
 
 Route::post('/amwalpay/callback', [WalletTopUpController::class, 'captureDataViaWebhook']);
 
-Route::get('/storage/{filename}', function ($filename) {
-    $path = '/home/u593542202/mednovacare.com/storage/app/public/' . $filename;
+Route::get('/storage/{folder}/{filename}', function ($folder, $filename) {
+    $path = storage_path("app/public/{$folder}/{$filename}");
 
-    if (!file_exists($path)) {
+    if (!File::exists($path)) {
         abort(404);
     }
 
-    return response()->file($path);
+    $file = File::get($path);
+    $type = File::mimeType($path);
+
+    return Response::make($file, 200)->header("Content-Type", $type);
 });
