aaac7e37d41586b2d67ae07f573281ae1358626a
nadaAbedAlkreem <elkaloutnada@gmail.com>
2026-01-19 11:22:29 +0200

    update configration

--- database/factories/CustomerFactory.php
+++ database/factories/CustomerFactory.php
@@ -15,27 +15,34 @@ class CustomerFactory extends Factory
      *
      * @return array<string, mixed>
      */
+    protected $model = Customer::class;
+
     public function definition(): array
     {
-        $type = fake()->randomElement(['therapist', 'rehabilitation_center', 'patient']);
+        $type = \fake()->randomElement([
+            'therapist',
+            'rehabilitation_center',
+            'patient'
+        ]);
 
         return [
-            'full_name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'phone' => fake()->phoneNumber(),
-            'gender' => fake()->randomElement(['Male', 'Female']),
+            'full_name' => \fake()->name(),
+            'email' => \fake()->unique()->safeEmail(),
+            'phone' => \fake()->phoneNumber(),
+            'gender' => \fake()->randomElement(['Male', 'Female']),
             'email_verified_at' => now(),
             'password' => bcrypt('password'),
-            'birth_date' => fake()->date('Y-m-d', '2005-01-01'),
-            'image' => fake()->imageUrl(200, 200, 'people'),
+            'birth_date' => \fake()->date('Y-m-d', '2005-01-01'),
+            'image' => \fake()->imageUrl(200, 200, 'people'),
             'provider' => null,
             'provider_id' => null,
             'fcm_token' => Str::random(10),
-            'is_online' => fake()->boolean(),
+            'is_online' => \fake()->boolean(),
             'last_active_at' => now(),
             'is_banned' => false,
             'type_account' => $type,
             'status' => 'active',
         ];
     }
+
 }
