1f504e69d66dff3081db77fd138ce51d5ff6355c
nadaAbedAlkreem <elkaloutnada@gmail.com>
2026-01-20 12:11:49 +0200

    edit(seeder) : add rating

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -64,15 +64,15 @@ class DatabaseSeeder extends Seeder
 //        });
 ////
 ////        // 🔹 أنشئ عملاء من نوع (Patient)
-//        Customer::factory(10)->create([
-//            'type_account' => 'patient',
-//        ])->each(function ($customer) {
-//            Patient::factory()->create([
-//                'customer_id' => $customer->id,
-//            ]);
-//
-//            Location::factory()->create(['customer_id' => $customer->id]);
-//        });
+        Customer::factory(10)->create([
+            'type_account' => 'patient',
+        ])->each(function ($customer) {
+            Patient::factory()->create([
+                'customer_id' => $customer->id,
+            ]);
+
+            Location::factory()->create(['customer_id' => $customer->id]);
+        });
 
 //        // 🔹 تقييمات تجريبية
 //        $this->call([RehabilitationCenterSeeder::class]);
